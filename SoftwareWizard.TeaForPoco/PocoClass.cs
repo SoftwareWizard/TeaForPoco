29d6a2b8554e22e169dfee7fac7badfc88267f46
Simon Bauer <simon.bauer@software-wizard.de>
2016-12-24 16:24:48 +0100

    added documentation

---
+++ SoftwareWizard.TeaForPoco/PocoClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareWizard.TeaForPoco
+{
+    public class PocoClass
+    {
+        public string Name { get; set; }
+
+        public IList<string> BaseClasses { get; set; }
+
+        public IList<string> BaseInterfaces { get; set; }
+
+        public IList<PocoProperty> PocoProperties { get; set; }
+    }
+}
---
+++ SoftwareWizard.TeaForPoco/PocoProperty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SoftwareWizard.TeaForPoco
+{
+    public class PocoProperty
+    {
+        public string Name { get; set; }
+
+        public TypeSyntax TypeSyntax { get; set; }
+    }
+}
